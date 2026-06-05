30d9a260245674a4a164c00efbb5e70a59a40560
Angela Solomons <angelasolomons03@gmail.com>
2026-06-05 15:52:54 +0200

    Added new functions

--- error_handling_helper_functions.js
+++ error_handling_helper_functions.js
@@ -1,3 +1,5 @@
+// Khanya G created functions validateInput(input) and testScore(number)
+// TODO: Create a function that validates user input 
 function validateInput(input) {
     if (typeof input !== 'string') {
         throw new Error('Input must be a string');
@@ -5,10 +7,12 @@ function validateInput(input) {
     if (input.trim() === '') {
         throw new Error('Input cannot be empty');
     }
-
     return true;
 }
+console.log(validateInput(453));
 
+
+// TODO: Create a function that demonstrates multiple error types 
 function testScore(number) {
     try {
         if (typeof number !== 'number') {
@@ -27,3 +31,30 @@ function testScore(number) {
     }
 }
 
+
+// Angela S created functions stringManipulation(str) and convertArray(arr) 
+// TODO: Create a collection of helper functions for string manipulation
+function stringManipulation(str) {
+    try {
+        if (typeof str !== "string") {
+            throw new Error("Input must be a string");
+        }
+        if (str.trim() === '') {
+            throw new Error("Input cannot be empty");
+        }
+        return str.toUpperCase();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+stringManipulation("frog");
+
+
+// TODO: Create helper functions for array operations 
+function convertArray(arr) {
+    let changedArray = arr.toString();
+    return console.log(changedArray);
+}
+
+const vehicleTYPES = ["car", "motorcycle", "truck"];
+convertArray(vehicleTYPES)
